34f6a4167e7a9f9e322b660c335b04745a57e98e
aymane-smi <aymanebel2@outlook.fr>
2023-08-04 16:58:59 +0100

    adding testing method for athors model

--- models/author.go
+++ models/author.go
@@ -3,50 +3,51 @@ package models
 import (
 	"github.com/aymane-smi/api-test/utils"
 	"fmt"
-	"go.uber.org/zap"
+	"errors"
+	//"go.uber.org/zap"
 )
 
 
 type Author struct{
-	id string
+	id int
 	full_name string
 
 }
 
-sampleJSON := []byte(`{
-	"level" : "info",
-	"encoding": "json",
-	"outputPaths":["log.log"],
-	"errorOutputPaths":["log.log"],
-	"encoderConfig": {
-		"messageKey":"message",
-		"levelKey":"level",
-		"levelEncoder":"lowercase"
-	}
-}`)
+// sampleJSON := []byte(`{
+// 	"level" : "info",
+// 	"encoding": "json",
+// 	"outputPaths":["log.log"],
+// 	"errorOutputPaths":["log.log"],
+// 	"encoderConfig": {
+// 		"messageKey":"message",
+// 		"levelKey":"level",
+// 		"levelEncoder":"lowercase"
+// 	}
+// }`)
 
-var cfg zap.Config
+// var cfg zap.Config
  
-   if err := json.Unmarshal(sampleJSON, &cfg); err != nil {
-       panic(err)
-   }
+//    if err := json.Unmarshal(sampleJSON, &cfg); err != nil {
+//        panic(err)
+//    }
  
-   logger, err := cfg.Build()
+//    logger, err := cfg.Build()
  
-   if err != nil {
-       panic(err)
-   }
-   defer logger.Sync()
+//    if err != nil {
+//        panic(err)
+//    }
+//    defer logger.Sync()
 
 func AddAuthor(a Author) (string, error){
 	db := utils.GetInstance()
 	stmt, err := db.Prepare("INSERT INTO authors(full_name) VALUES($1)")
 	if err != nil{
-		logger.Warn(err)
+		//logger.Warn(err)
 		return "", err
 	}
 	if _, err := stmt.Exec(a.full_name); err != nil{
-		logger.Warn(err)
+		//logger.Warn(err)
 		return "", err
 	}
 	return "new row inserted in authors", nil
@@ -57,7 +58,7 @@ func UpdateAuthor(a Author) (*Author, error){
 	db := utils.GetInstance()
 	stmt, err := db.Prepare("UPDATE authors SET full_name= $1 WHERE id = $2")
 	if err != nil {
-		logger.Warn(err)
+		//logger.Warn(err)
 		return nil, err
 	}
 
@@ -72,14 +73,14 @@ func GetAuthorById(id int) *Author{
 	db := utils.GetInstance()
 	stmt, err := db.Prepare("SELECT * FROM authors WHERE id = $1")
 	if err != nil{
-		logger.Warn(err)
+		//logger.Warn(err)
 		return nil
 	}
 
 	row := stmt.QueryRow(id)
 	var tmp_author Author
 	if err := row.Scan(&tmp_author.id, &tmp_author.full_name); err != nil{
-		logger.Warn(err)
+		//logger.Warn(err)
 		return nil
 	}
 	return &tmp_author
@@ -90,12 +91,15 @@ func DeleteAuthorById(id int) (string, error){
 	db := utils.GetInstance()
 	stmt, err := db.Prepare("DELETE FROM authors WHERE id = $1")
 	if err != nil{
-		logger.Warn(err)
+		fmt.Println("prepare error")
+		//logger.Warn(err)
 		return "", err
 	}
-	if _, err := stmt.Exec(id); err != nil{
-		logger.Warn(err)
-		return "", err
+	x, err := stmt.Exec(id);
+	rowsAffected, err := x.RowsAffected()
+	if rowsAffected == 0 || err != nil{
+		//logger.Warn(err)
+		return "", errors.New("invalid id")
 	}
 
 	return fmt.Sprintf("book with id '%d' was deleted", id), nil
--- models/author_test.go
+++ models/author_test.go
@@ -6,13 +6,36 @@ import (
 )
 
 func TestAddAuthor(t *testing.T){
-	fmt.Println("test author")
 	tmp := Author{
 		full_name: "test test",
 	}
 	message, err := AddAuthor(tmp)
 
 	if message == "" && err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestUpdateAuthor(t *testing.T){
+	tmp := Author{
+		id: 1,
+		full_name: "test* test*",
+	}
+
+	if author, err := UpdateAuthor(tmp); author == nil && err != nil{
+		t.Errorf("%v", err)
+	}
+}
+
+func TestGetAuthorById(t *testing.T){
+	if author := GetAuthorById(1); author == nil{
+		fmt.Println(author)
+		t.Errorf("invalid id")
+	}
+}
+
+func TestDeleteAuthorById(t *testing.T){
+	if msg, err := DeleteAuthorById(3); msg == "" && err != nil{
 		t.Errorf("%v", err)
 	}
 }

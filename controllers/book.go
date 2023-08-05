795eb4eaff625acb6eada95eb5863932accc76b9
aymane-smi <aymanebel2@outlook.fr>
2023-08-05 15:09:06 +0100

    making controller for books

--- controllers/book.go
+++ controllers/book.go
@@ -1,17 +1,97 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/aymane-smi/api-test/models"
 	"github.com/aymane-smi/api-test/utils"
 	"github.com/gorilla/mux"
 )
 
-r := mux.NewRouter();
-
 func GetBookById(w http.ResponseWriter, r *http.Request){
-	utils.JsonHeader()
 
-	vars := mux.vars(r)
+	vars := mux.Vars(r)
 
-	
+	book := models.GetBookById(vars["id"])
+
+	if book == nil {
+		response := map[string]interface{}{
+			"message": "invalid book id",
+		}
+		jsonResponse, _ := json.Marshal(response)
+		utils.JsonWriter(w, http.StatusNotFound, jsonResponse)
+		return
+	}
+
+	response := map[string]interface{}{
+		"book": book,
+	}
+	jsonResponse, _ := json.Marshal(response)
+	utils.JsonWriter(w, http.StatusOK, jsonResponse)
+}
+
+func AddBook(w http.ResponseWriter, r *http.Request){
+	var book models.Book
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	msg, err := models.AddBook(book)
+
+	if err != nil{
+		response := map[string]interface{}{
+			"message": "something went wrong",
+		}
+		jsonResponse, _ := json.Marshal(response)
+		utils.JsonWriter(w, http.StatusInternalServerError, jsonResponse)
+		return
+	}
+	response := map[string]interface{}{
+		"message": msg,
+	}
+	jsonResponse, _ := json.Marshal(response)
+	utils.JsonWriter(w, http.StatusOK, jsonResponse)
+
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request){
+	var book models.Book
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	b, err := models.UpdateBook(book)
+
+	if err != nil{
+		response := map[string]interface{}{
+			"message": "something went wrong",
+		}
+		jsonResponse, _ := json.Marshal(response)
+		utils.JsonWriter(w, http.StatusInternalServerError, jsonResponse)
+		return
+	}
+	response := map[string]interface{}{
+		"book": b,
+	}
+	jsonResponse, _ := json.Marshal(response)
+	utils.JsonWriter(w, http.StatusOK, jsonResponse)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+
+	msg, err := models.DeleteById(vars["id"])
+
+	if err != nil{
+		response := map[string]interface{}{
+			"message": "invalid book id",
+		}
+		jsonResponse, _ := json.Marshal(response)
+		utils.JsonWriter(w, http.StatusInternalServerError, jsonResponse)
+		return
+	}
+	response := map[string]interface{}{
+		"message": msg,
+	}
+	jsonResponse, _ := json.Marshal(response)
+	utils.JsonWriter(w, http.StatusOK, jsonResponse)
 }
--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aymane-smi/api-test/controllers"
+	"github.com/aymane-smi/api-test/utils"
+	"github.com/gorilla/mux"
+)
+
+func main(){
+
+	r := mux.NewRouter()
+
+	utils.InitLogger()
+
+	r.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+
+	r.HandleFunc("/book", controllers.AddBook).Methods("POST")
+
+	r.HandleFunc("/book", controllers.UpdateBook).Methods("PUT")
+
+	r.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
+
+	fmt.Println("Start listening to server at port 8000")
+
+	http.ListenAndServe(":8000", r)
+}
